e68dde217bafd4e4acfa7549c407df73d49e25bd
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-03-25 22:11:31 +0300

    Initial commit. Adds gradle dependencies

---
+++ app/src/main/java/com/beloushkin/fooddataviewer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.beloushkin.fooddataviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
