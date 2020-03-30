4afbeec7e9fe15d25af50061b187024d83b04da5
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-03-30 18:51:15 +0400

    Adds food details fragment layout

--- app/src/main/java/com/beloushkin/fooddataviewer/fooddetails/FoodDetailsFragment.kt
+++ app/src/main/java/com/beloushkin/fooddataviewer/fooddetails/FoodDetailsFragment.kt
@@ -2,12 +2,13 @@ package com.beloushkin.fooddataviewer.fooddetails
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.beloushkin.fooddataviewer.R
 import com.beloushkin.fooddataviewer.getViewModel
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 
 
-class FoodDetailsFragment: Fragment() {
+class FoodDetailsFragment: Fragment(R.layout.food_details_fragment) {
 
     private val args: FoodDetailsFragmentArgs by navArgs()
 
